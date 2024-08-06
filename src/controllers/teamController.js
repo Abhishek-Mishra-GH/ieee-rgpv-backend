f4a67474210400a88762f93816c905401a0e4146
Abhishek Mishra <cs.abhishekmo@gmail.com>
2024-08-06 19:55:48 +0530

    init

--- src/controllers/teamController.js
+++ src/controllers/teamController.js
@@ -1,34 +1,34 @@
-const Team = require('../models/teamModel');
+const Team = require("../models/teamModel");
 
 exports.createTeam = async (req, res) => {
-    try {
-        const { teamName, teamLeader, email, contact, collegeName, branch, year, startupIdea, mediumOfReach, teamMembers } = req.body;
-    
-        const newTeam = new Team({
-        teamName,
-        teamLeader,
-        email,
-        contact,
-        collegeName,
-        branch,
-        year,
-        startupIdea,
-        mediumOfReach,
-        teamMembers
-        });
-    
-        await newTeam.save();
-        res.status(201).json(newTeam);
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
-}
+  try {
+    const { teamName, teamLeader, email, contact, collegeName, branch, year, startupIdea, mediumOfReach, teamMembers } = req.body;
+
+    const newTeam = new Team({
+      teamName,
+      teamLeader,
+      email,
+      contact,
+      collegeName,
+      branch,
+      year,
+      startupIdea,
+      mediumOfReach,
+      teamMembers,
+    });
+
+    await newTeam.save();
+    res.status(201).json(newTeam);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
 
 exports.getTeams = async (req, res) => {
-    try {
-        const teams = await Team.find();
-        res.status(200).json(teams);
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
-}
+  try {
+    const teams = await Team.find();
+    res.status(200).json(teams);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
